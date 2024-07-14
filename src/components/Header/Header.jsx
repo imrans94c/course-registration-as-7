ae6e37396fba43b3d2dbd3b34218ef15eed37e8b
imrans94c <imrans94c@gmail.com>
2024-07-14 08:41:31 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-[#1C1B1B]">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
